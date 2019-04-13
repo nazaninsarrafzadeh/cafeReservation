f1c76a02ab9b72ada63428adf8cf737f11a454f2
nazaninsarrafzadeh <43335937+nazaninsarrafzadeh@users.noreply.github.com>
2019-04-13 22:03:01 +0430

    some changes in db structure

--- src/main/java/model/DAO/UserDAO.java
+++ src/main/java/model/DAO/UserDAO.java
@@ -1,15 +1,12 @@
 package model.DAO;
 
-
 import controller.security.PasswordUtils;
 import model.DTO.User;
 
 import java.sql.*;
 import java.util.ArrayList;
 
-/**
- * Created by HP on 1/31/2019.
- */
+
 public class UserDAO {
 
     private int count=0;
@@ -41,6 +38,25 @@ public class UserDAO {
         }
     }
 
+    public void update(User user){
+        try {
+            PreparedStatement statement=connection.prepareStatement
+                    ("UPDATE users SET name=?,lastname=?,email=?,image_path=?,filename=?,bio=? WHERE uid = ?");
+            statement.setString(1,user.getName());
+            statement.setString(2,user.getLastname());
+            statement.setString(3,user.getEmail());
+            statement.setString(4,user.getImagePath());
+            statement.setString(5,user.getImageName());
+            statement.setString(6,user.getBio());
+            statement.setInt(7,user.getId());
+            statement.execute();
+        } catch (SQLException e) {
+            System.out.println("couldn't update!!!");
+            e.printStackTrace();
+        }
+    }
+
+
     public boolean emailExists(String email){
         try {
             PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE email=?");
@@ -57,53 +73,58 @@ public class UserDAO {
 
     public boolean loginCheck(String email,String userPass){
         try {
-                PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE email=?");
-                statement.setString(1,email);
-                ResultSet set=statement.executeQuery();
-                if (set.next()){
-                    String password = set.getString("password");
-                    String salt = set.getString("salt");
-                    boolean passwordMatch = PasswordUtils.verifyUserPassword(userPass, password, salt);
+            PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE email=?");
+            statement.setString(1,email);
+            ResultSet set=statement.executeQuery();
+            if (set.next()){
+                String password = set.getString("password");
+                String salt = set.getString("salt");
+                boolean passwordMatch = PasswordUtils.verifyUserPassword(userPass, password, salt);
 
-                    if(passwordMatch)
-                    {
-                        return true;
-                    }
+                if(passwordMatch)
+                {
+                    return true;
                 }
-            } catch (SQLException e) {
-                System.out.println("could not select");
             }
-            return false;
+        } catch (SQLException e) {
+            System.out.println("could not select");
+        }
+        return false;
     }
-//    public boolean loginCheck(String email,String password){
-//
-//            try {
-//                PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE email=? AND password=?");
-//                statement.setString(1,email);
-//                statement.setString(2,password);
-//                ResultSet set=statement.executeQuery();
-//                if (set.next()){
-//                    return true;
-//                }
-//            } catch (SQLException e) {
-//                System.out.println("could not select");
-//            }
-//            return false;
-//    }
 
     public int getCustomerId(String email){
         int id=0;
         try {
-            PreparedStatement statement=connection.prepareStatement("SELECT ID FROM users WHERE email=?");
+            PreparedStatement statement=connection.prepareStatement("SELECT uid FROM users WHERE email=?");
             statement.setString(1,email);
             ResultSet set=statement.executeQuery();
             if (set.next()){
-                id = set.getInt("ID");
+                id = set.getInt("uid");
             }
         } catch (SQLException e) {
             System.out.println("could not select");
         }
         return id;
+    }
+
+    public User getUserById(int id){
+        User users=new User();
+        try {
+            PreparedStatement statement=connection.prepareStatement("SELECT * FROM users WHERE UID=?");
+            statement.setInt(1,id);
+            ResultSet set=statement.executeQuery();
+            if (set.next()){
+                users.setName(set.getString("name"));
+                users.setLastname(set.getString("lastname"));
+                users.setEmail(set.getString("email"));
+                users.setImageName(set.getString("filename"));
+                users.setBio(set.getString("bio"));
+            }
+            System.out.println(count);
+        } catch (SQLException e) {
+            System.out.println("could not select");
+        }
+        return users;
     }
 
     public ArrayList<User> getRecords(){
@@ -123,5 +144,35 @@ public class UserDAO {
             System.out.println("could not select");
         }
         return records;
+    }
+    public int calculateFollowers(int uid){
+        int followers=0;
+        try{
+        PreparedStatement statement=connection.prepareStatement("select COUNT(uid2) from followU where uid2=? GROUP BY uid2 ");
+            statement.setInt(1,uid);
+            ResultSet set=statement.executeQuery();
+            if (set.next()){
+                followers = set.getInt("count");
+            }
+
+        }catch (SQLException e){
+            System.out.println("can't calculate");
+        }
+        return followers;
+    }
+    public int calculateFollowing(int uid){
+        int following=0;
+        try{
+            PreparedStatement statement=connection.prepareStatement("select COUNT(uid1) from followU where uid1=? GROUP BY uid1");
+            statement.setInt(1,uid);
+            ResultSet set=statement.executeQuery();
+            if (set.next()){
+                following = set.getInt("count");
+            }
+
+        }catch (SQLException e){
+            System.out.println("can't calculate");
+        }
+        return following;
     }
 }
